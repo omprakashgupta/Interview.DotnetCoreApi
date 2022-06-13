80a7e4c0ac5ac7295cfbe39ea107f2b8634c6c0e
OM Gupta <om.siplservices.com>
2022-06-13 08:29:37 +0530

    1. Work on StartupFilter middleware

---
+++ Interview.DotnetCoreApi/Middleware/Filter/RequestSetOptionsMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Interview.DotnetCoreApi.Middleware.Filter
+{
+	public class RequestSetOptionsMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public RequestSetOptionsMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		// Test with https://localhost:5001/Privacy/?option=Hello
+		public async Task Invoke(HttpContext context)
+		{
+			var option = context.Request.Query["option"];
+			if (!string.IsNullOrWhiteSpace(option))
+			{
+				context.Items["option"] = WebUtility.HtmlEncode(option);
+			}
+			await _next(context);
+		}
+	}
+}
---
+++ Interview.DotnetCoreApi/Middleware/Filter/RequestSetOptionsStartupFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace Interview.DotnetCoreApi.Middleware.Filter
+{
+	public class RequestSetOptionsStartupFilter : IStartupFilter
+	{
+		public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+		{
+			return builder =>
+			{
+				builder.UseMiddleware<RequestSetOptionsMiddleware>();
+				next(builder);
+			};
+		}
+	}
+}
